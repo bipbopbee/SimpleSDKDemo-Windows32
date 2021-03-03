6592edaec0b3a3879ac8dd7fe0c763dc30fae097
sushuai <516854715@qq.com>
2021-03-03 16:44:35 +0800

    增加角度旋转

--- 播放/SimplePlayerSDKWin32Demo/SimplePlayerSDKWin32Demo/SDK/SimplePlayerSDK.h
+++ 播放/SimplePlayerSDKWin32Demo/SimplePlayerSDKWin32Demo/SDK/SimplePlayerSDK.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "windows.h"
+#include <thread>
 class __declspec(dllexport) SimplePlayerSDK
 {
 public:
+	void setRotate(int degree);
 	void setPlayUrl(char* url);
 	void setPlayWnd(HWND wnd);
 	void setIsPlaying(bool playing);
+	void setBufferSize(int buffersize);
 	char* getPlayUrl();
 	HWND getPlayWnd();
 	bool getIsPlaying();
@@ -15,5 +18,6 @@ public:
 	~SimplePlayerSDK();
 private:
 	void* m_player;
+	std::thread m_authorthread;
 };
 
